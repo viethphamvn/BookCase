012d5ef1dc2faaadf8811e24efe025e7a38ef4fb
Viet Pham <tug36669@temple.edu>
2019-10-23 10:25:20 -0400

    added bookTitle ArrayList

--- app/src/main/java/edu/temple/bookcase/MainActivity.java
+++ app/src/main/java/edu/temple/bookcase/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<String> bookTitle = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.bookTitle)));
+
+        
+
     }
 }
